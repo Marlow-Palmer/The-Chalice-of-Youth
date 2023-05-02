f3718f4f7b0e19e01f9c78a5e8c15e1c7c735473
Marlow <126528017+Marlow-Palmer@users.noreply.github.com>
2023-05-02 18:04:10 -0500

    did a little more work.
    trying to figure out collisions. adding to my scripts

--- Assets/Scripts/ChaliceButtonScript.cs
+++ Assets/Scripts/ChaliceButtonScript.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class ChaliceButtonScript : MonoBehaviour
 {
     public UnityEngine.UI.Button chaliceButton;
     public PlayerImmortality playerDamageScript;
+    public TextMeshProUGUI Output;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +21,13 @@ public class ChaliceButtonScript : MonoBehaviour
     public void CollectChalice()
     { 
         Debug.Log("CollectChalice");
-        //playerDamageScript.CollectChalice();
-        //chaliceButton.interactable = false;
-        //chaliceButton.onClick.AddListener(DestroyButton);
+        playerDamageScript.CollectChalice();
+        chaliceButton.interactable = false;
+        chaliceButton.onClick.AddListener(DestroyButton);
+        Output.text = " ";
     }
 
-    void DestroyButton()
+    public void DestroyButton()
     {
         Destroy(gameObject);
         Debug.Log("Player collected The Chalice");
--- Assets/Scripts/QuestCompleteScript.cs
+++ Assets/Scripts/QuestCompleteScript.cs
@@ -3,26 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class QuestCompleteScript : MonoBehaviour
 {
     public TextMeshProUGUI Output;
     private int numClicks = 0;
+    public PlayerImmortality playerDamageScript;
 
     public void NextButton()
     {
-        numClicks = numClicks + 1;
-        if (numClicks == 1)
+        if (playerDamageScript.isImmortal != true)
         {
-            Output.text = "What was that noise???";
+            Output.text = "You have not completed your quest yet.";
         }
-        else if (numClicks == 2)
+        else
         {
-            Output.text = "The Kingdom is under attack! You must go and protect us!";
-        }
-        else if (numClicks == 3)
-        {
-            SceneManager.LoadScene("GameOverScene");
+            numClicks = numClicks + 1;
+            if (numClicks == 1)
+            {
+                Output.text = "What was that noise???";
+            }
+            else if (numClicks == 2)
+            {
+                Output.text = "The Kingdom is under attack! You must go and protect us!";
+            }
+            else if (numClicks == 3)
+            {
+                SceneManager.LoadScene("GameOverScene");
+            }
         }
     }
 }
